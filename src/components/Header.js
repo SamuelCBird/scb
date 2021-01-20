0219015c8e23f1663137399c91b78f98bd8aef2e
SBird <samuel@samuelcbird.co.uk>
2021-01-20 22:17:58 +0000

    experimenting with responsiveness

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { BrowserView, MobileView, isBrowser, isMobile } from 'react-device-detect';
 import '../styles/Header.css';
 
 export const Header = () => {
@@ -7,10 +8,10 @@ export const Header = () => {
         <div id='header'>
             <div id='titleMainNav'>
                 <h1>
-                    <NavLink className='link' exact to='/'>samuelcbird</NavLink>
+                    <NavLink className='link' exact to='/'>{ isMobile ? 'scb' : 'samuelcbird' }</NavLink>
                 </h1>
                 <ul id="navList">
-                    <NavLink className='link' activeClassName='activeLink' to='/music'><li id="musicListItem">music</li></NavLink>
+                    <NavLink className='link' activeClassName='activeLink' to='/music'><li id="musicListItem">{ isBrowser ? 'null' : 'music' }</li></NavLink>
                     <NavLink className='link' activeClassName='activeLink' to='/photography'><li id="photogListItem">photography</li></NavLink>
                     <NavLink className='link' activeClassName='activeLink' to='/programming'><li id="progListItem">programming</li></NavLink>
                 </ul>
