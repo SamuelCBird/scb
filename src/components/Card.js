d0c3af17130cb278d097febe115b33d2cc403297
SBird <samuel@samuelcbird.co.uk>
2021-01-23 19:47:56 +0000

    added more responsiveness

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,7 +6,7 @@ export const Card = (props) => {
     
     let cardStyle = {
         position: 'relative',
-        margin: '10px 10px 0 0',
+        margin: '5px',
         border: '5px solid #fff',
         width: '160px',
         height: '160px',
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
-import { isMobile, isMobileOnly } from 'react-device-detect';
 import '../styles/Header.css';
 import music_svg from '../images/music.svg';
 import photo_svg from '../images/photo.svg';
 import prog_svg from '../images/prog.svg';
 
 export const Header = () => {
+
+    const [dimensions, setDimensions] = useState({
+        height: window.innerHeight, 
+        width: window.innerWidth
+    })
+
+    function debounce(fn, ms) {
+        let timer
+        return _ => {
+            clearTimeout(timer)
+            timer = setTimeout(_ => {
+                timer = null
+                fn.apply(this, arguments)
+            }, ms)
+        }
+    }
+
+    useEffect(() => {
+        const debouncedHandleResize = debounce(function handleResize () {
+            setDimensions({
+                height: window.innerHeight,
+                width: window.innerWidth
+            })
+        }, 500)
+
+        window.addEventListener('resize', debouncedHandleResize)
+
+        return _ => {
+            window.removeEventListener('resize', debouncedHandleResize)
+        }
+    })
+
     return (
         <div id='header'>
             <div id='titleMainNav'>
                 <h1>
-                    <NavLink className='link' exact to='/'>{ isMobile ? 'scb' : 'samuelcbird' }</NavLink>
+                    <NavLink className='link' exact to='/'>{ dimensions.width < 960 ? 'scb' : 'samuelcbird' }</NavLink>
                 </h1>
                 <ul id="navList">
                     <NavLink className='link' activeClassName='activeLink' to='/music'>
-                        <li><div className="listItem" style={{backgroundImage: `url(${music_svg})`}}></div><div>{ isMobile ? null : 'music'}</div></li>
+                        <li><div className="listItem" style={{backgroundImage: `url(${music_svg})`}}></div><div>{ dimensions.width < 960 ? null : 'music'}</div></li>
                     </NavLink>
                     <NavLink className='link' activeClassName='activeLink' to='/photography'>
-                        <li><div className='listItem' style={{backgroundImage: `url(${photo_svg})`}}></div><div>{ isMobile ? null : 'photography'}</div></li>
+                        <li><div className='listItem' style={{backgroundImage: `url(${photo_svg})`}}></div><div>{ dimensions.width < 960 ? null : 'photography'}</div></li>
                     </NavLink>
                     <NavLink className='link' activeClassName='activeLink' to='/programming'>
-                        <li><div className='listItem' style={{backgroundImage: `url(${prog_svg})`}}></div><div>{ isMobile ? null : 'programming'}</div></li>
+                        <li><div className='listItem' style={{backgroundImage: `url(${prog_svg})`}}></div><div>{ dimensions.width < 960 ? null : 'programming'}</div></li>
                     </NavLink>
                 </ul>
             </div>
