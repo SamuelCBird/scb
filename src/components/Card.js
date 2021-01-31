c00ec53ae2637180a1da6e4d1f1f9bbf7f125482
SamuelCBird <samuel@samuelcbird.co.uk>
2021-01-31 14:12:49 +0000

    modulised more css

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,22 +1,12 @@
 import React from 'react';
 import { cats, cardSize } from './DataEnums'
-// import '../styles/Card.css'
+import '../styles/Card.css'
 
 export const Card = (props) => {
     
     let cardStyle = {
-        position: 'relative',
-        margin: '5px',
-        border: '5px solid #fff',
         width: '160px',
-        height: '160px',
-        filter: 'drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))',
         backgroundImage: '',
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: 'cover',
-        backgroundPosition: 'top',
-        borderRadius: '25px',
-        overflow: 'hidden'
     }
     let line = {
         position: 'absolute',
