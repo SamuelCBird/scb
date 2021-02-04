3a7c9acd3dea267beb319d0872449a48a8b61c3a
SBird <samuel@samuelcbird.co.uk>
2021-02-04 21:58:35 +0000

    sorted main site, started work on cv

--- src/components/CurriculumVitae.js
+++ src/components/CurriculumVitae.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import '../styles/cv.css';
 
 export const CurriculumVitae = () => {
     return (
-        <div className=''>
-            Curriculum Vitae.
+        <div id='wrapper'>
+            <div id='cv_content'>
+                <p>samuel@samuelcbird.co.uk</p>
+                <p>+44 7546 708 420</p>
+                <h1>Samuel Charles Bird</h1>
+                <h2>I am awesome.</h2>
+                <p>Yo yo yo</p>
+            </div>
         </div>
     )
 }
