fa77558b61a0b22d01d9d8b431e857c7af43b2c5
SBird <samuel@samuelcbird.co.uk>
2021-02-25 17:59:16 +0000

    Revert "updated git ignore"
    This reverts commit ef368be4c5aa2bb03f42f6a63510837c187b7033.

--- src/components/ContentWithDB.js
+++ src/components/ContentWithDB.js
@@ -1,14 +1,32 @@
 import React, { useEffect, useState } from 'react';
-import { db, storage } from './Firestore';
+import db from './Firestore';
 
 const ContentWithDB = () => {
-    
+    const [dbData, setDbData] = useState([])
+
+    useEffect(() => {
+        db.collection('portfolio_data').get().then(snapshot => {
+            snapshot.forEach(item => {
+                setDbData(prevItems => [
+                    ...prevItems,
+                    item.data().title, 
+                    item.data().description
+                ])
+            })
+        })
+    }, [])
 
     return (
 
         <div id="wrapper">
             <div className="content">
-
+                <ul>
+                    {
+                        dbData.map(tit => {
+                            return <li>{tit}</li>
+                        })
+                    }
+                </ul>
             </div>
         </div>
     )
--- src/components/Firestore.js
+++ src/components/Firestore.js
@@ -3,20 +3,15 @@
 import firebase from "firebase/app";
 // Add the Firebase services that you want to use
 import "firebase/firestore";
-import "firebase/storage";
 
 // Initialize Cloud Firestore through Firebase
 firebase.initializeApp({
-    apiKey: "AIzaSyBWnboNmVpm1hVSMDKrHKSTYhGYc0E4Wzg",
-    authDomain: "samuelcbird-5c11a.firebaseapp.com",
-    projectId: "samuelcbird-5c11a",
-    storageBucket: "samuelcbird-5c11a.appspot.com",
-    messagingSenderId: "764693052984",
-    appId: "1:764693052984:web:693dc36ba680e23c8fb0cd"
+    apiKey: 'AIzaSyBWnboNmVpm1hVSMDKrHKSTYhGYc0E4Wzg',
+    authDomain: 'samuelcbird-5c11a.firebaseapp.com',
+    projectId: 'samuelcbird-5c11a'
 });
 
 var db = firebase.firestore();
-var storage = firebase.storage().ref()
 
-export {db, storage};
+export default db;
 
