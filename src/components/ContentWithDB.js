ef368be4c5aa2bb03f42f6a63510837c187b7033
SBird <samuel@samuelcbird.co.uk>
2021-02-25 17:48:15 +0000

    updated git ignore

--- src/components/ContentWithDB.js
+++ src/components/ContentWithDB.js
@@ -1,32 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import db from './Firestore';
+import { db, storage } from './Firestore';
 
 const ContentWithDB = () => {
-    const [dbData, setDbData] = useState([])
-
-    useEffect(() => {
-        db.collection('portfolio_data').get().then(snapshot => {
-            snapshot.forEach(item => {
-                setDbData(prevItems => [
-                    ...prevItems,
-                    item.data().title, 
-                    item.data().description
-                ])
-            })
-        })
-    }, [])
+    
 
     return (
 
         <div id="wrapper">
             <div className="content">
-                <ul>
-                    {
-                        dbData.map(tit => {
-                            return <li>{tit}</li>
-                        })
-                    }
-                </ul>
+
             </div>
         </div>
     )
--- src/components/Firestore.js
+++ src/components/Firestore.js
@@ -3,15 +3,21 @@
 import firebase from "firebase/app";
 // Add the Firebase services that you want to use
 import "firebase/firestore";
+import "firebase/storage";
 
 // Initialize Cloud Firestore through Firebase
 firebase.initializeApp({
-    apiKey: 'AIzaSyBWnboNmVpm1hVSMDKrHKSTYhGYc0E4Wzg',
-    authDomain: 'samuelcbird-5c11a.firebaseapp.com',
-    projectId: 'samuelcbird-5c11a'
+    apiKey: "AIzaSyBWnboNmVpm1hVSMDKrHKSTYhGYc0E4Wzg",
+    authDomain: "samuelcbird-5c11a.firebaseapp.com",
+    projectId: "samuelcbird-5c11a",
+    storageBucket: "samuelcbird-5c11a.appspot.com",
+    messagingSenderId: "764693052984",
+    appId: "1:764693052984:web:693dc36ba680e23c8fb0cd"
 });
 
 var db = firebase.firestore();
+var storage = firebase.storage().ref()
 
-export default db;
+export {db, storage};
 
+// wah wah 
